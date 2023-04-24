7f4b64b656b2c83eef817d31493b84b3274ed8cc
Luiz Lima <lima.luizgo@gmail.com>
2023-04-24 19:44:12 -0300

    Novo update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-// import { CadastroComponent } from './cadastro/cadastro.component';
-// import { InicioComponent } from './inicio/inicio.component';
-// import { LoginComponent } from './login/login.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [// { path: 'cadastro', component: CadastroComponent },
-  // { path: 'login', component: LoginComponent },
-  // { path: 'inicio', component: InicioComponent }
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)], exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { HeaderComponent } from './components/header/header.component';
-import { MainComponent } from './components/main/main.component';
-import { AsideComponent } from './components/aside/aside.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { MainComponent } from './shared/main/main.component';
+import { AsideComponent } from './shared/aside/aside.component';
+import { FooterComponent } from './shared/footer/footer.component';
 import { NgOptimizedImage } from '@angular/common';
+import { SearchComponent } from './shared/aside/search/search.component';
+import { MapComponent } from './shared/aside/map/map.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HotelsComponent } from './shared/main/hotels/hotels.component';
+import { HotelComponent } from './shared/main/hotel/hotel.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, MainComponent, AsideComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    AsideComponent,
+    FooterComponent,
+    SearchComponent,
+    MapComponent,
+    LoginComponent,
+    RegisterComponent,
+    HotelsComponent,
+    HotelComponent,
+    HomeComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, ReactiveFormsModule, NgOptimizedImage],
   providers: [],
   bootstrap: [AppComponent],
