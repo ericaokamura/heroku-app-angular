2d55e7d12e8400facbd219a5e1df5f24764e0104
Luiz Lima <lima.luizgo@gmail.com>
2023-04-27 15:58:49 -0300

    Update user.service.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,28 +1,27 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { User } from '../interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  usersUrl: string = 'http://localhost:8080/usuarios';
+  private readonly apiUrl = 'http://localhost:8080/usuarios';
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-  getUser(id: number): Observable<User> {
-    const url: string = `${this.usersUrl}/${id}`;
-    return this.httpClient.get<User>(url);
+  public getUserById(id: number): Observable<User> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.get<User>(url);
   }
 
-  getUserByEmail(email: string): Observable<User> {
-    const url: string = `${this.usersUrl}/email/${email}`;
-    return this.httpClient.get<User>(url);
+  public getUserByEmail(email: string): Observable<User> {
+    const url = `${this.apiUrl}/email/${email}`;
+    return this.http.get<User>(url);
   }
 
-  postUser(user: User): Observable<User> {
-    const url: string = `${this.usersUrl}/`;
-    return this.httpClient.post<User>(url, user);
+  public postUser(user: User): Observable<User> {
+    return this.http.post<User>(this.apiUrl, user);
   }
 }
