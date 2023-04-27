82d96cd243d8f792f9ad4a83953fd4b2976d40ca
Luiz Lima <lima.luizgo@gmail.com>
2023-04-27 14:02:01 -0300

    Login validação

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  loginForm: FormGroup = new FormGroup({});
 
+  constructor(private formBuilder: FormBuilder, private userService: UserService) {
+    this.createForm();
+  }
+
+  onSubmit(): void {}
+
+  private createForm(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/),
+        ],
+      ],
+    });
+  }
 }
