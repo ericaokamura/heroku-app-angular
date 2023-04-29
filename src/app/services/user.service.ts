ae80ae92fc11a696b3f3c1a89f6b8ce0cc09aa1e
Luiz Lima <lima.luizgo@gmail.com>
2023-04-29 09:17:13 -0300

    Update user.service.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 import { User } from '../interfaces/user.interface';
 
 @Injectable({
@@ -19,37 +20,55 @@ export class UserService {
 
   public getUserById(id: number): Observable<User> {
     const url = `${this.apiUrl}/${id}`;
-    return this.http.get<User>(url, this.httpOptions);
+    return this.http.get<User>(url, this.httpOptions).pipe(
+      retry(3), // Tentativas máximas de 3
+      catchError(this.handleError) // Trata erros
+    );
   }
 
   public getUserByEmail(email: string): Observable<User> {
     const url = `${this.apiUrl}/email/${email}`;
-    return this.http.get<User>(url, this.httpOptions);
+    return this.http.get<User>(url, this.httpOptions).pipe(retry(3), catchError(this.handleError));
   }
 
   public postUser(user: User): Observable<User> {
-    return this.http.post<User>(this.apiUrl, user, this.httpOptions);
+    return this.http.post<User>(this.apiUrl, user, this.httpOptions).pipe(retry(3), catchError(this.handleError));
   }
 
   public updateUser(user: User): Observable<User> {
     const url = `${this.apiUrl}/${user.id}`;
-    return this.http.put<User>(url, user, this.httpOptions);
+    return this.http.put<User>(url, user, this.httpOptions).pipe(retry(3), catchError(this.handleError));
   }
 
   public deleteUser(id: number): Observable<any> {
     const url = `${this.apiUrl}/${id}`;
-    return this.http.delete(url, this.httpOptions);
+    return this.http.delete(url, this.httpOptions).pipe(retry(3), catchError(this.handleError));
   }
 
   public getAllUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.apiUrl, this.httpOptions);
+    return this.http.get<User[]>(this.apiUrl, this.httpOptions).pipe(retry(3), catchError(this.handleError));
   }
 
   public createUser(user: User): Observable<User> {
-    return this.http.post<User>(`${this.apiUrl}`, user, this.httpOptions);
+    return this.http.post<User>(`${this.apiUrl}`, user, this.httpOptions).pipe(retry(3), catchError(this.handleError));
   }
 
   public updateUserById(id: number, user: User): Observable<User> {
-    return this.http.put<User>(`${this.apiUrl}/${id}`, user, this.httpOptions);
+    return this.http
+      .put<User>(`${this.apiUrl}/${id}`, user, this.httpOptions)
+      .pipe(retry(3), catchError(this.handleError));
+  }
+
+  private handleError(error: any) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side error
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
   }
 }
